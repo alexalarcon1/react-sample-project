6bfc28d424d296b68eb5d15b5c1a544d1325e5c6
Alex Alarcon <alexalarcon@consultinggroupinternational.com>
2017-02-19 14:59:30 -0500

    Projects, ProjectItem and AddProject components implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
-import Projects from './Components/Projects'
+import uuid from 'uuid';
+import Projects from './Components/Projects';
+import AddProject from './Components/AddProject';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        My App
-        <Projects />
-      </div>
-    );
-  }
+	constructor(){
+		super();
+		this.state = {
+			projects: []
+		}
+	}
+
+	//Lifecycle method
+	componentWillMount(){
+		this.setState({projects: [
+			{
+				id: uuid.v4(),
+				title: "Business Website",
+				category: "Web Design"
+			},
+			{
+				id: uuid.v4(),
+				title: "Social App",
+				category: "Mobile Dev"
+			},
+			{
+				id: uuid.v4(),
+				title: "Ecommerce Website",
+				category: "Web Dev"
+			}
+		]});
+	}
+
+	//Updates projects array
+	handleAddProject(project){
+		let projects = this.state.projects;
+		projects.push(project);
+		this.setState({projects:projects})
+	}
+
+	handleOnDeleteProject(id){
+		let projects = this.state.projects;
+		let index = projects.findIndex(x => x.id === id);
+		projects.splice(index, 1);
+		this.setState({projects:projects})
+	}
+
+	render() {
+	    return (
+	      <div className="App">
+	        <AddProject addProject={this.handleAddProject.bind(this)}/>
+	        <Projects projects={this.state.projects} onDelete={this.handleOnDeleteProject.bind(this)}/>
+	      </div>
+	    );
+	}
 }
 
 export default App;
